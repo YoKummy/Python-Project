052948add447c0aaff5404aa731658e0ee55921b
YoKummy <80161555+YoKummy@users.noreply.github.com>
2024-03-21 21:05:03 +0800

    V1

---
+++ FileSortingBot.py
@@ -0,0 +1,22 @@
+import glob, os, shutil, sched, time
+download = '/Users/User/Downloads/'
+os.chdir(download)
+
+event_schedule = sched.scheduler(time.time, time.sleep)
+
+filelist = ["pdf", "png", "jpg", "pptx", "zip", "mov", "cube", "mp3", "mp4", "raw", "jar", 
+            "exe", "7z", "docx", "xlsx"]
+
+for ext in filelist:
+    if not os.path.isdir(ext):
+        os.mkdir(ext)
+
+    dfiles = [f for f in os.listdir() if f.lower().endswith("." + ext)]
+
+    for dfile in dfiles:
+            src_path = os.path.join(download, dfile)
+            dest_path = os.path.join(download, ext, dfile)
+            shutil.move(src_path, dest_path)
+
+""" for file in glob.glob("*.jar"):
+    print(file) """
